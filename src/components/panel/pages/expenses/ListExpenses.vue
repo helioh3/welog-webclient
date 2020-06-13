9057d21d0fe597fb7dbb5e9fd5c5fa3d9d6a6751
Helio Brito <helioh3@Helios-MacBook-Pro.local>
2020-06-13 01:21:01 -0300

    [feature] Adicionado interacao com checkbox

--- src/components/panel/pages/expenses/ListExpenses.vue
+++ src/components/panel/pages/expenses/ListExpenses.vue
@@ -48,13 +48,13 @@
                 </div>
 
             </div>
-
+            
             <table class="table-data">
                 <thead>
                     <tr>
                         <th>
                             <label>
-                                 <input type="checkbox" class="field__checkbox">
+                                 <input type="checkbox" class="field__checkbox" @click="selectAll($event)">
                             </label>
                         </th>
                         <th>#numero</th>
@@ -70,10 +70,18 @@
                     <!-- <router-link class="tabs__item tabs__item--active" :to="{name: 'painel.categorias', params:{id: category.id}}">
                         
                     </router-link> -->
-                    <tr v-for="expense in expenses.data" :key="expense.id" @dblclick="edit(expense.id)">
+                    <tr :class="{focused: focused === expense.id }" 
+                        v-for="expense in expenses.data" 
+                        :key="expense.id" 
+                        @click.stop="focus(expense.id)" 
+                        @dblclick="edit(expense.id)"
+                    >
                         <td>
                              <label>
-                                 <input type="checkbox" class="field__checkbox">
+                                <input type="checkbox" class="field__checkbox" 
+                                    @click.stop="select(expense.id, $event.target.checked)" 
+                                    :checked="selected.includes(expense.id)"
+                                >
                             </label>
                         </td>
                         <td>{{ expense.numero }}</td>
@@ -111,7 +119,9 @@
 
         data() {
             return {
-                search: ''
+                search: '',
+                selected: [],
+                focused: ''
             }
         },
         
@@ -120,7 +130,7 @@
                 return this.$store.state.expenses.items
             },
 
-            params () {
+            params() {
                 return {
                     page: this.expenses.current_page,
                     filter: this.search
@@ -146,13 +156,81 @@
                     .catch(error => {
                         this.$snotify.error('Algo errado ao visualizar', 'Erro')
                     })
+            },
+
+            select(id, checked) {
+                
+                if(checked){
+                    this.selected.push(id)
+                    return
+                }
+                const index = this.selected.findIndex( (element) => id === element )
+                
+                //Early return tecnica
+                if(index < 0 ){
+                    return
+                }
+                this.selected.splice(index, 1)
+            },
+
+            selectAll($event) {
+                if ($event.target.checked){
+                        this.selected = this.expenses.data.map( (expense) => expense.id)
+                     
+                     return 
+                }
+
+                this.selected = []
+            },
+
+            focus (id){
+                this.focused = id
+            },
+            
+            onPress ($event){
+                // console.log($event.keyCode)
+                if(!this.focused){
+                    
+                    return
+                }
+
+                if($event.keyCode === 32){
+                    this.select(this.focused, !this.selected.includes(this.focused) )
+
+                    return
+                }
+
+                let index = this.expenses.data.findIndex( (element) => this.focused === element.id)
+                
+                if ($event.keyCode === 38) {
+                    index--
+                } else if ($event.keyCode === 40) {
+                    index++
+                }
+
+                if(!this.expenses.data[index]){
+                    
+                    return
+                }
+
+                this.focused = this.expenses.data[index].id
             }
+        
         },
 
         components: {
            paginate: PaginationGeneral, 
            search: SearchPanel
           
+        },
+
+        mounted() {
+            document.addEventListener("keyup", this.onPress)
+
+        },
+
+        destroyed() {
+            document.removeEventListener("keyup", this.onPress)
         }
         
     }
