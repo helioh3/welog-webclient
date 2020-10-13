d94de4615635a39283db3c392e61c8903f7c5687
Helio Brito <helioh3@gmail.com>
2020-10-13 16:02:33 -0300

    [feature: beta: 1.0.1] fix installments data test

--- src/components/share/Form/AppDate.vue
+++ src/components/share/Form/AppDate.vue
@@ -3,15 +3,15 @@
     {{ formatValue(value) }}
   </div>
   <v-date-picker
-    v-else
-    :value="receivedValue"
-    @input="emitValue"
-    locale="pt"
-    :input-props="{
-      placeholder: 'dd/mm/AAAA',
-      class:
-        'appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4',
-    }"
+	v-else
+	:value="receivedValue"
+	@input="emitValue"
+	locale="pt"
+	:input-props="{
+		placeholder: 'dd/mm/AAAA',
+		class:
+		'appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4',
+	}"
   />
 </template>
 
@@ -37,12 +37,12 @@ export default {
    */
 computed: {
 /**
-     * @return {Date}
-     */
+ * @return {Date}
+ */
     receivedValue() {
-      if (this.value && typeof this.value === "string") {
-        return new Date(this.value);
-      }
+	if (this.value && typeof this.value === "string") {
+	return new Date(this.value);
+	}
 
       if (this.value instanceof Date) {
         return this.value;
