ce3a8df3e350df8dd0d2ffd203c90cd47295cceb
Helio Brito <helioh3@gmail.com>
2020-11-09 07:34:11 -0300

    [feature: beta: 1.0.1] fixs

--- src/components/panel/pages/expenses/ListExpenses.vue
+++ src/components/panel/pages/expenses/ListExpenses.vue
@@ -97,6 +97,7 @@
                       :checked="selected.includes(expense.id)"
                     >
                   </td>
+				  
 
                   <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                     <div class="text-sm leading-5 text-gray-900">{{ expense.id }}</div>
