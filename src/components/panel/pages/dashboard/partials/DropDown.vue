887b94d547e344c5f74c64df8a62100e6a21fe7b
Helio Brito <helioh3@gmail.com>
2020-11-16 00:02:22 -0300

    [feature: beta: 1.0.1] add images users, provideres

--- src/components/panel/pages/dashboard/partials/DropDown.vue
+++ src/components/panel/pages/dashboard/partials/DropDown.vue
@@ -6,7 +6,7 @@
 	>
 		<img
 			class="h-full w-full object-cover"
-			src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=296&q=80"
+			src="@/assets/images/user.png"
 			alt="Your avatar"
 		/>
 	</button>
--- src/components/panel/pages/expenses/ListExpenses.vue
+++ src/components/panel/pages/expenses/ListExpenses.vue
@@ -112,7 +112,7 @@
                       <div class="flex-shrink-0 h-10 w-10">
                         <img
                           class="h-10 w-10 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          src="@/assets/images/provider.png"
                           alt=""
                         />
                       </div>
