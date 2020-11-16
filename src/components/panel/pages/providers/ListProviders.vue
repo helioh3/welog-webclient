03dfa8b880d26f42a78d93af206aaa63859b21f1
Helio Brito <helioh3@gmail.com>
2020-11-16 00:04:24 -0300

    [feature: beta: 1.0.1] add images users, provideres

--- src/components/panel/pages/providers/ListProviders.vue
+++ src/components/panel/pages/providers/ListProviders.vue
@@ -68,7 +68,7 @@
 								<td class="px-6 py-2 whitespace-no-wrap border-b border-gray-200">
 									<div class="flex items-center">
 										<div class="flex-shrink-0 h-10 w-10">
-											<img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+											<img class="h-10 w-10 rounded-full" src="@/assets/images/provider.png" alt="" />
 										</div>
 
 										<div class="ml-4">
