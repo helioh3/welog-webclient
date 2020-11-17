eebf76af47248a9404ebd7faca4ecb5870a5585f
Helio Brito <helioh3@gmail.com>
2020-11-17 17:17:33 -0300

    [feature: beta: 1.0.1] fix form company

--- src/components/panel/pages/companies/partials/FormCompany.vue
+++ src/components/panel/pages/companies/partials/FormCompany.vue
@@ -55,9 +55,9 @@
 				
 				<div class="md:w-1/3 px-3 mb-6 md:mb-0">
 					<label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
-						CNPJ *
+						CNPJ / CPF *
 					</label>
-					<input v-model="company.cnpj" v-mask="'##.###.###/####-##'" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"  type="text" placeholder="CNPJ">
+					<input v-model="company.cnpj" v-mask="['##.###.###/####-##', '###.###.###-##']" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"  type="text" placeholder="CNPJ">
 				</div>
 				<div class="md:w-1/3 px-3 mb-6 md:mb-0">
 					<label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
