98400c45edf0742b0820473db9ad6496c81381ff
Helio Brito <helioh3@gmail.com>
2020-11-19 13:40:02 -0300

    [feature: beta: 1.0.1] fix search providers

--- src/components/panel/pages/bankaccounts/ListBankaccounts.vue
+++ src/components/panel/pages/bankaccounts/ListBankaccounts.vue
@@ -67,7 +67,8 @@
 								</td>
 								<td class="px-6 py-3 whitespace-no-wrap border-b border-gray-200">
 									<div class="text-sm leading-5 text-gray-900">
-										{{bankaccount.abank.Name}}
+										{{ bankaccount ? bankaccount.abank.Name : '' }}
+										<!-- {{bankaccount.abank.Name}} -->
 									</div>
 								</td>
 
