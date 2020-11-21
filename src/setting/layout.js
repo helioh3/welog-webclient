f312abab39c75c80afe37fe2601b98885895dc20
William Correa <wilcorrea@gmail.com>
2020-11-21 11:50:59 -0300

    [feature: beta: 1.0.1] Configurar consumo de fornecedores

---
+++ src/setting/layout.js
@@ -0,0 +1,14 @@
+/**
+ * @type {number}
+ */
+export const TABLE_HEIGHT_UP = 320
+
+/**
+ * @type {number}
+ */
+export const TABLE_HEIGHT_DOWN = 80
+
+/**
+ * @type {number}
+ */
+export const TABLE_HEIGHT_CELL = 55
