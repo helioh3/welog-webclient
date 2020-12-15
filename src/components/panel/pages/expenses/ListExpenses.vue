12521e8dcc214e9b3c1e938d278c25be609f87e5
Helio Brito <helioh3@gmail.com>
2020-12-14 21:35:31 -0300

    [feature: beta: 1.0.1] wil, pull

--- src/components/panel/pages/expenses/ListExpenses.vue
+++ src/components/panel/pages/expenses/ListExpenses.vue
@@ -71,6 +71,9 @@
                   <th class="px-6 py-3 border-b border-gray-400 bg-gray-100 text-left text-xs leading-4 font-semi-bold text-gray-700 uppercase tracking-wider">
                     Vencimento
                   </th>
+				  <th class="px-6 py-3 border-b border-gray-400 bg-gray-100 text-left text-xs leading-4 font-semi-bold text-gray-700 uppercase tracking-wider">
+                    Parcelas
+                  </th>
                   <th class="px-6 py-3 border-b border-gray-400 bg-gray-100 text-left text-xs leading-4 font-semi-bold text-gray-700 uppercase tracking-wider">
                     status
                   </th>
@@ -124,10 +127,10 @@
 
                       <div class="ml-4">
                         <div
-                          class="text-sm leading-5 font-medium text-gray-900"
+                          class="text-sm leading-5 font-medium text-gray-900 overflow-hidden truncate w-48"
                           :title="expense.provider.fantasia"
                         >
-                          {{ expense.provider.nome }}
+                          {{ expense.provider.fantasia }}
                         </div>
                         <!-- <div class="text-sm leading-5 text-gray-500">(63) 992099291</div> -->
                       </div>
@@ -148,6 +151,10 @@
                       class-names="text-lg leading-5 text-gray-600"
                       readonly
                     />
+                  </td>
+
+				  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                   2/3
                   </td>
 
                   <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
